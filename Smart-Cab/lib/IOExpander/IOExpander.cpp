a6bd3c26e56599c94328d22d8f3fe6e16d6d4f63
Ganney Dortch <ganneyd@gmail.com>
2020-05-27 20:57:55 -0400

    Redo the IOExpander class and inherited the PCF8574 class, provided a global IOExpander class to be used by LED and FAN/DEHUMIDFIER Class

--- Smart-Cab/lib/IOExpander/IOExpander.cpp
+++ Smart-Cab/lib/IOExpander/IOExpander.cpp
@@ -1,22 +1,18 @@
 #include "IOExpander.h"
 
-IOExpander::IOExpander()
-    :s_expander(EXPANDER_ADDR){
-        //initialize the IC
-        begin();
-    }
 
-IOExpander::begin(){
-    for(int i=0; i<NUM_OF_IO_PINS; i++){
-  s_expander.pinMode(i,OUTPUT);
- }
- 
- //start up the i/o expander for use
- s_expander.begin();
+IOExpander::IOExpander(){
+    IOExpander::begin();
 }
 
-IOExpander::digitalWrite(uint8_t pin, uint8_t value){
-    if(pin<NUM_OF_IO_PINS){
-    s_expander.digitalWrite(pin,value);
+void IOExpander::begin(){
+    for(int i=0; i<8;i++){
+        pinMode(i,OUTPUT);
     }
 }
+
+void IOExpander:: IO_digitalWrite(uint8_t pin, uint8_t value){
+    PCF8574::digitalWrite(pin,value);
+}
+
+IOExpander globalExpander = IOExpander();
--- Smart-Cab/lib/IOExpander/IOExpander.h
+++ Smart-Cab/lib/IOExpander/IOExpander.h
@@ -1,35 +1,12 @@
-#pragma once 
+#pragma once
 
-
-/*
-This class manages the I/O Expander and handles all outputs/inputs 
-to/from the PCF8574 IC. 
-Class provides a reliable way to communicate with the IC 
-
-0 to 5 are reserved for LED
-6 & 7 are reserved for Fan and Dehumidifier 
-
-*/
-
-#define EXPANDER_ADDR 0x3E
-#define NUM_OF_IO_PINS 8
-//include Libray for IC
 #include "PCF8574.h"
+#include "../Config/Config.h"
 
-class IOExpander{
-    private:
+struct IOExpander:protected PCF8574
 
-    //All instance of the class will share the same IC
-    static PCF8574 s_expander;
-     //Set all pinmodes (8) to output as all are going to be desinated outputs
-    void begin();
     public:
-
-    //Constructor
     IOExpander();
-
-  
-
-    //Function to toggle pins
-    void digitalWrite(uint8_t pin, uint8_t value);
+    void begin();
+    void IO_digitalWrite(uint8_t pin, uint8_t value);
 };
