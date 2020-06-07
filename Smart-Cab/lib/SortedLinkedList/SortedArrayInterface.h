fea71f2f1418cc8ab7dacaba7b413c56364e50d3
Ganney Dortch <ganneyd@gmail.com>
2020-06-07 14:46:31 -0400

    deleted SortedArrayInterface as it is not used

--- Smart-Cab/lib/SortedLinkedList/SortedArrayInterface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//make abstract class so that we can use various classes in sorted array and be certain that they 
-//have this method
-class SortedArrayInterface{
-    //classes that inherit this class has to implement this function
-    virtual const char* getName() = 0;
-}
